cb95cb6cf8e79ed94291287854439e839ab2adb1
Michael Freeman <mfreeman451@gmail.com>
2025-03-18 12:38:08 -0500

    cleanup

--- web/src/env.js
+++ web/src/env.js
@@ -23,5 +23,5 @@ export const env = createRuntimeEnv({
   serverOnly: ['API_KEY', 'AUTH_ENABLED'],
   
   // Optional: List any client-side variables
-  clientSide: ['NEXT_PUBLIC_API_URL', 'NEXT_PUBLIC_AUTH_ENABLED', 'AUTH_ENABLED'],
+  clientSide: ['NEXT_PUBLIC_API_URL'],
 });
