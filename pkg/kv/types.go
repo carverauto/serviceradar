be5152796c380e1fba46e817710d65822423dfd1
Michael Freeman <mfreeman451@gmail.com>
2025-03-25 22:42:50 -0500

    cleanup

--- pkg/kv/types.go
+++ pkg/kv/types.go
@@ -17,9 +17,6 @@
 package kv
 
 import (
-	"fmt"
-	"path/filepath"
-
 	"github.com/carverauto/serviceradar/pkg/models"
 )
 
@@ -46,94 +43,4 @@ type Config struct {
 		Roles []RBACRule `json:"roles"`
 	} `json:"rbac"`
 	Bucket string `json:"bucket,omitempty"` // Added for NATS KV bucket
-}
-
-var (
-	errListenAddrRequired = fmt.Errorf("listen_addr is required")
-	errNatsURLRequired    = fmt.Errorf("nats_url is required")
-	errSecurityRequired   = fmt.Errorf("security configuration is required for mTLS")
-	errCertFileRequired   = fmt.Errorf("tls.cert_file is required for mTLS")
-	errKeyFileRequired    = fmt.Errorf("tls.key_file is required for mTLS")
-	errCAFileRequired     = fmt.Errorf("tls.ca_file is required for mTLS")
-)
-
-// Validate ensures the configuration is valid.
-func (c *Config) Validate() error {
-	if err := c.validateRequiredFields(); err != nil {
-		return err
-	}
-
-	if err := c.validateSecurity(); err != nil {
-		return err
-	}
-
-	c.normalizeCertPaths()
-	c.setDefaultBucket()
-
-	return nil
-}
-
-// validateRequiredFields checks for mandatory top-level fields.
-func (c *Config) validateRequiredFields() error {
-	if c.ListenAddr == "" {
-		return errListenAddrRequired
-	}
-
-	if c.NatsURL == "" {
-		return errNatsURLRequired
-	}
-
-	return nil
-}
-
-// validateSecurity ensures security settings are valid.
-func (c *Config) validateSecurity() error {
-	if c.Security == nil || c.Security.Mode != "mtls" {
-		return errSecurityRequired
-	}
-
-	tls := c.Security.TLS
-
-	if tls.CertFile == "" {
-		return errCertFileRequired
-	}
-
-	if tls.KeyFile == "" {
-		return errKeyFileRequired
-	}
-
-	if tls.CAFile == "" {
-		return errCAFileRequired
-	}
-
-	return nil
-}
-
-// normalizeCertPaths prepends CertDir to relative TLS file paths.
-func (c *Config) normalizeCertPaths() {
-	certDir := c.Security.CertDir
-	if certDir == "" {
-		return
-	}
-
-	tls := &c.Security.TLS
-
-	if !filepath.IsAbs(tls.CertFile) {
-		tls.CertFile = filepath.Join(certDir, tls.CertFile)
-	}
-
-	if !filepath.IsAbs(tls.KeyFile) {
-		tls.KeyFile = filepath.Join(certDir, tls.KeyFile)
-	}
-
-	if !filepath.IsAbs(tls.CAFile) {
-		tls.CAFile = filepath.Join(certDir, tls.CAFile)
-	}
-}
-
-// setDefaultBucket assigns a default bucket name if none is specified.
-func (c *Config) setDefaultBucket() {
-	if c.Bucket == "" {
-		c.Bucket = "serviceradar-kv"
-	}
 }
