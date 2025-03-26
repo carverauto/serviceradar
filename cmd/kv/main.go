bf630a9f41c58c0ac8d685f7be1bd198c63cd56f
Michael Freeman <mfreeman451@gmail.com>
2025-03-25 21:45:52 -0500

    properly starting up now

--- cmd/kv/main.go
+++ cmd/kv/main.go
@@ -1,19 +1,3 @@
-/*
- * Copyright 2025 Carver Automation Corporation.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package main
 
 import (
@@ -24,6 +8,8 @@ import (
 	"github.com/carverauto/serviceradar/pkg/config"
 	"github.com/carverauto/serviceradar/pkg/kv"
 	"github.com/carverauto/serviceradar/pkg/lifecycle"
+	"github.com/carverauto/serviceradar/proto"
+	ggrpc "google.golang.org/grpc"
 )
 
 func main() {
@@ -33,7 +19,6 @@ func main() {
 
 	ctx := context.Background()
 
-	// Load KV service config
 	cfgLoader := config.NewConfig()
 
 	var cfg kv.Config
@@ -41,32 +26,33 @@ func main() {
 		log.Fatalf("Failed to load config: %v", err)
 	}
 
-	// Override NatsURL if provided via flag
 	if *natsURL != "" {
 		cfg.NatsURL = *natsURL
 	}
 
-	// Validate the configuration
 	if err := cfg.Validate(); err != nil {
 		log.Fatalf("Invalid configuration: %v", err)
 	}
 
-	// Create KV server (store is created internally)
-	server, err := kv.NewServer(ctx, cfg)
+	server, err := kv.NewServer(ctx, &cfg)
 	if err != nil {
 		log.Fatalf("Failed to create KV server: %v", err)
 	}
 
-	// Set KV store for config package
 	cfgLoader.SetKVStore(server.Store())
 
-	// Run with lifecycle management
 	opts := &lifecycle.ServerOptions{
 		ListenAddr:        cfg.ListenAddr,
 		ServiceName:       "kv",
 		Service:           server,
 		EnableHealthCheck: true,
 		Security:          cfg.Security,
+		RegisterGRPCServices: []lifecycle.GRPCServiceRegistrar{
+			func(srv *ggrpc.Server) error {
+				proto.RegisterKVServiceServer(srv, server)
+				return nil
+			},
+		},
 	}
 
 	if err := lifecycle.RunServer(ctx, opts); err != nil {
