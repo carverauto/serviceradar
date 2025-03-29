b450b20227c8229b123e877a103b52db85c0919a
Michael Freeman <mfreeman451@gmail.com>
2025-03-28 20:01:50 -0500

    fixing doc

--- scripts/buildAll.sh
+++ scripts/buildAll.sh
@@ -20,6 +20,7 @@ VERSION=${VERSION:-1.0.27}
 ./scripts/setup-deb-agent.sh
 ./scripts/setup-deb-poller.sh
 ./scripts/setup-deb-kv.sh
+./scripts/setup-deb-nats.sh
 ./scripts/setup-deb-sync.sh
 ./scripts/setup-deb-web.sh
 ./scripts/setup-deb-dusk-checker.sh
@@ -37,6 +38,6 @@ scp ./release-artifacts/serviceradar-snmp-checker_${VERSION}.deb duskadmin@192.1
 scp ./release-artifacts/serviceradar-poller_${VERSION}.deb 192.168.2.23:~/
 scp ./release-artifacts/serviceradar-agent_${VERSION}.deb 192.168.2.23:~/
 scp ./release-artifacts/serviceradar-kv_${VERSION}.deb 192.168.2.23:~/
+scp ./release-artifacts/serviceradar-nats_${VERSION}.deb 192.168.2.23:~/
 scp ./release-artifacts/serviceradar-sync_${VERSION}.deb 192.168.2.23:~/
-scp ./release-artifacts/serviceradar-dusk-checker_${VERSION}.deb 192.168.2.23:~/
 scp ./release-artifacts/serviceradar-snmp-checker_${VERSION}.deb 192.168.2.23:~/
