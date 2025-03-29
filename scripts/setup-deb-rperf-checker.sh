2e88f8fe544ac760a965f2477ac1e9689dfaaf7e
Michael Freeman <mfreeman451@gmail.com>
2025-03-29 15:01:19 -0500

    adding back proto builder

--- scripts/setup-deb-rperf-checker.sh
+++ scripts/setup-deb-rperf-checker.sh
@@ -41,6 +41,11 @@ mkdir -p "${TEMP_DIR}/lib/systemd/system"
 
 # Build the plugin
 echo "Building Rust rperf plugin..."
+protoc -I=proto \
+    --go_out=proto --go_opt=paths=source_relative \
+    --go-grpc_out=proto --go-grpc_opt=paths=source_relative \
+    proto/rperf/rperf.proto
+
 cd cmd/checkers/rperf
 cargo build --release
 
