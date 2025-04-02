b94784e127e755a666c5e2ddc5a45dbe4c100b9a
Michael Freeman <mfreeman451@gmail.com>
2025-04-02 15:14:12 -0500

    WIP

--- web/next.config.ts
+++ web/next.config.ts
@@ -33,10 +33,12 @@ const nextConfig: NextConfig = {
       },
     ];
   },
+  /*
   env: {
     NEXT_PUBLIC_API_URL:
       process.env.NEXT_PUBLIC_API_URL || "http://localhost:8090",
   },
+   */
   serverRuntimeConfig: {
     // Will only be available on the server side
     apiKey: process.env.API_KEY || "",
