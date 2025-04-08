bf89e941c82ec4317e3a00edc3230279d5de8a45
Michael Freeman <mfreeman451@gmail.com>
2025-04-08 18:24:52 -0500

    bump

--- pkg/core/server.go
+++ pkg/core/server.go
@@ -342,7 +342,7 @@ func (s *Server) sendStartupNotification(ctx context.Context) error {
 		Timestamp: time.Now().UTC().Format(time.RFC3339),
 		PollerID:  "core",
 		Details: map[string]any{
-			"version":  "1.0.29",
+			"version":  "1.0.30",
 			"hostname": getHostname(),
 		},
 	}
