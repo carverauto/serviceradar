9dbe00486f9c2d7e963a694859912fbc6db98ec0
Michael Freeman <mfreeman451@gmail.com>
2025-04-24 18:10:23 -0500

    fixing nats

--- packaging/nats/scripts/postinstall.sh
+++ packaging/nats/scripts/postinstall.sh
@@ -11,6 +11,11 @@ if ! id -u serviceradar >/dev/null 2>&1; then
     useradd --system --no-create-home --shell /usr/sbin/nologin -g serviceradar serviceradar
 fi
 
+# Create NATS user if it doesn't exist
+if ! id -u nats >/dev/null 2>&1; then
+    useradd --system --no-create-home --shell /usr/sbin/nologin -g serviceradar nats
+fi
+
 # Create required directories
 mkdir -p /etc/serviceradar
 mkdir -p /var/lib/serviceradar
@@ -23,7 +28,6 @@ fi
 # Set permissions
 chown -R serviceradar:serviceradar /etc/serviceradar
 chmod -R 755 /etc/serviceradar
-usermod -a -G serviceradar nats
 # if the certs dir exists, set permissions
 if [ -d "/etc/serviceradar/certs" ]; then
     chown -R serviceradar:serviceradar /etc/serviceradar/certs
