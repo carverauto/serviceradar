8eaff04ba2be77b6398c8b2be7e8972845276031
Michael Freeman <mfreeman451@gmail.com>
2025-05-09 23:46:08 -0500

    change to correct library for nats

--- pkg/consumers/netflow/processor.go
+++ pkg/consumers/netflow/processor.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"net"
 
-	"github.com/nats-io/nats.go"
+	"github.com/nats-io/nats.go/jetstream"
 	flowpb "github.com/netsampler/goflow2/v2/pb"
 	"google.golang.org/protobuf/proto"
 )
@@ -19,11 +19,11 @@ func NewProcessor() *Processor {
 }
 
 // Process processes a single NetFlow message.
-func (p *Processor) Process(msg *nats.Msg) error {
+func (p *Processor) Process(msg jetstream.Msg) error {
 	// Parse protobuf message
 	var flow flowpb.FlowMessage
 
-	if err := proto.Unmarshal(msg.Data, &flow); err != nil {
+	if err := proto.Unmarshal(msg.Data(), &flow); err != nil {
 		return fmt.Errorf("failed to unmarshal FlowMessage: %w", err)
 	}
 
@@ -33,7 +33,7 @@ func (p *Processor) Process(msg *nats.Msg) error {
 
 	// Log flow details (placeholder for actual processing)
 	log.Printf("Processed NetFlow message on subject %s: Type=%s, SrcAddr=%s, DstAddr=%s, SrcPort=%d, DstPort=%d, Bytes=%d",
-		msg.Subject, flow.Type.String(), srcAddr, dstAddr, flow.SrcPort, flow.DstPort, flow.Bytes)
+		msg.Subject(), flow.Type.String(), srcAddr, dstAddr, flow.SrcPort, flow.DstPort, flow.Bytes)
 
 	// TODO: Implement actual NetFlow processing
 	// e.g., Store in database, send to analytics, etc.
