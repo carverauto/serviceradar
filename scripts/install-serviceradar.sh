adbcc40f943ffecf77a0987b0d83fc716de09386
Michael Freeman <mfreeman451@gmail.com>
2025-05-14 10:39:15 -0500

    new release

--- scripts/install-serviceradar.sh
+++ scripts/install-serviceradar.sh
@@ -20,8 +20,8 @@
 set -e
 
 # Configuration
-VERSION="1.0.37"
-RELEASE_TAG="1.0.37"
+VERSION="1.0.38"
+RELEASE_TAG="1.0.38"
 RELEASE_URL="https://github.com/carverauto/serviceradar/releases/download/${RELEASE_TAG}"
 TEMP_DIR="/tmp/serviceradar-install"
 POLLER_CONFIG="/etc/serviceradar/poller.json"
