8fe345e3443a1a4f8ca588a428bc816d50352fd4
Michael Freeman <mfreeman451@gmail.com>
2025-05-22 22:45:47 -0500

    interfaces cleanup for metricstore

--- pkg/metricstore/interfaces.go
+++ pkg/metricstore/interfaces.go
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2025 Carver Automation Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Package metricstore pkg/metricstore/interfaces.go
 package metricstore
 
 import (
@@ -6,16 +23,15 @@ import (
 	"time"
 )
 
+//go:generate mockgen -destination=mock_metricstore.go -package=metricstore github.com/carverauto/serviceradar/pkg/metricstore RperfManager,SNMPManager
+
 // RperfManager defines the interface for managing rperf metrics.
 type RperfManager interface {
-	StoreRperfMetric(ctx context.Context, pollerID string, metric *models.RperfMetric, timestamp time.Time) error // Use RperfMetric as the input
+	StoreRperfMetric(ctx context.Context, pollerID string, metric *models.RperfMetric, timestamp time.Time) error
 	GetRperfMetrics(ctx context.Context, pollerID string, startTime, endTime time.Time) ([]*models.RperfMetric, error)
 }
 
 // SNMPManager defines the interface for managing SNMP metrics.
 type SNMPManager interface {
 	GetSNMPMetrics(ctx context.Context, nodeID string, startTime, endTime time.Time) ([]models.SNMPMetric, error)
-	// If you later add a specific StoreSNMPMetric, it would go here.
-	// For now, SNMP data is collected by the agent and sent as generic TimeseriesMetrics
-	// or handled by the `snmp.SNMPService` internally.
 }
