f035de871dc011b90ee13dcf39f1b3194b6893c5
Michael Freeman <mfreeman451@gmail.com>
2025-05-31 23:42:44 -0500

    adding mock

--- pkg/mapper/interfaces.go
+++ pkg/mapper/interfaces.go
@@ -14,6 +14,8 @@
  * limitations under the License.
  */
 
+//go:generate mockgen -destination=mock_mapper.go -package=mapper github.com/carverauto/serviceradar/pkg/mapper Mapper,Publisher
+
 // Package mapper pkg/discovery/interfaces.go
 package mapper
 
