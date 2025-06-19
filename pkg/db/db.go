3cf4be4260f8e3445613880168c2cec804064174
Michael Freeman <mfreeman451@gmail.com>
2025-06-19 15:18:26 -0500

    moving migrations

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -350,13 +350,13 @@ func getVersionedKVStreamStatements() []string {
           device_id string,
           ip string,
           poller_id string,
-          hostname Nullable(string),
-          mac Nullable(string),
+          hostname nullable(string),
+          mac nullable(string),
           discovery_source string,
           is_available boolean,
           first_seen DateTime64(3),
           last_seen DateTime64(3),
-          metadata Map(string, string),
+          metadata map(string, string),
           agent_id string
        )
        PRIMARY KEY (device_id)
