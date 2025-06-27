64597046502adfa3b836dbecd06dbdefabbd76a0
Michael Freeman <mfreeman451@gmail.com>
2025-06-26 21:03:24 -0500

    cleanup

--- pkg/db/migrations/20250626500000_populate_unified_devices_with_services.down.sql
+++
@@ -1,4 +0,0 @@
--- Rollback script for populating unified_devices with service devices
--- Remove all service devices from unified_devices
-
-DELETE FROM unified_devices WHERE device_type = 'service_device';
