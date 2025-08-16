efcb61f268051254d2f2289ebbd7b0eca8c8b7c0
Michael Freeman <mfreeman451@gmail.com>
2025-08-16 14:49:19 -0500

    WIP proton connection working but not NATS

--- docker/compose/fix-cert-permissions.sh
+++ docker/compose/fix-cert-permissions.sh
@@ -9,7 +9,8 @@ chmod 644 /etc/serviceradar/certs/*-key.pem  # Make private keys readable by own
 
 # Make certificates readable by group (for Proton which runs as uid=999, gid=999)  
 chgrp -R 1000 /etc/serviceradar/certs/
-chmod g+r /etc/serviceradar/certs/*.pem
-chmod g+r /etc/serviceradar/certs/*-key.pem
+chmod 755 /etc/serviceradar/certs/
+chmod 644 /etc/serviceradar/certs/*.pem
+chmod 644 /etc/serviceradar/certs/*-key.pem  # Make private keys readable by all for container use
 
 echo "✅ Certificate permissions fixed for ServiceRadar components (1000:1000)"
