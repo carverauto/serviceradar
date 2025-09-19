03f9cbdab300dde983c701cce74bc47b69c06900
Michael Freeman <mfreeman451@gmail.com>
2025-09-18 19:46:28 -0500

    adding packaging/core addl. files

---
+++ web/next.config.bazel.mjs
@@ -0,0 +1,31 @@
+/*-
+ * Copyright 2025 Carver Automation Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const nextConfig = {
+  reactStrictMode: true,
+  output: "standalone",
+  env: {
+    NEXT_PUBLIC_API_URL:
+      process.env.NEXT_PUBLIC_API_URL || "http://localhost:8090",
+    // WebSocket authentication uses cookies only - no API keys in query parameters
+  },
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    apiKey: process.env.API_KEY || "",
+  },
+};
+
+export default nextConfig;
