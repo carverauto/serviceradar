cf3cb51473a5a3255cc01c7d62fefcc14a87def7
Michael Freeman <mfreeman451@gmail.com>
2025-09-30 21:34:05 -0500

    cleanup

--- third_party/vendor/tools_opam/tools/xxd.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -x
-
-cd lib/templates;
-find ocamlsdk -name "*.BUILD" -depth 1 -exec xxd -i {} {}.c \;
-find compiler_libs -name "*.BUILD" -depth 1 -exec xxd -i {} {}.c \;
-find toolchain -name "*.BUILD" -depth 1 -exec xxd -i {} {}.c \;
