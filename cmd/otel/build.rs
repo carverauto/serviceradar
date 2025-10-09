7f4359caf80d161faffa521ecb67539a8bbc3810
Michael Freeman <mfreeman451@gmail.com>
2025-10-09 02:40:23 -0500

    WIP - proton still choking

--- cmd/otel/build.rs
+++ cmd/otel/build.rs
@@ -1,12 +1,15 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
+        .protoc_arg("--experimental_allow_proto3_optional")
         .disable_comments(".") // Disable comments to avoid doctest issues
         .compile_protos(
             &[
                 "proto/opentelemetry/proto/collector/trace/v1/trace_service.proto",
                 "proto/opentelemetry/proto/collector/logs/v1/logs_service.proto",
+                "proto/opentelemetry/proto/collector/metrics/v1/metrics_service.proto",
                 "proto/opentelemetry/proto/trace/v1/trace.proto",
                 "proto/opentelemetry/proto/logs/v1/logs.proto",
+                "proto/opentelemetry/proto/metrics/v1/metrics.proto",
                 "proto/opentelemetry/proto/resource/v1/resource.proto",
                 "proto/opentelemetry/proto/common/v1/common.proto",
             ],
@@ -20,6 +23,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     };
     let kv_proto_dir = std::path::Path::new(kv_proto_path).parent().unwrap();
     tonic_build::configure()
+        .protoc_arg("--experimental_allow_proto3_optional")
         .build_server(false)
         .build_client(true)
         .compile_protos(&[kv_proto_path], &[kv_proto_dir])?;
