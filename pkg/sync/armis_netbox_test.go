c26834821faa953ce28266fcc391323024b7d768
Michael Freeman <mfreeman451@gmail.com>
2025-10-21 12:24:23 -0500

    fixed linter errors

--- pkg/sync/armis_netbox_test.go
+++ pkg/sync/armis_netbox_test.go
@@ -309,7 +309,8 @@ func TestGetResultsWithArmisAndNetBox(t *testing.T) {
 	hasNetBox := false
 
 	for _, device := range devices {
-		switch device.Source { //nolint:exhaustive
+		//nolint:exhaustive // only need Armis/NetBox for this assertion matrix.
+		switch device.Source {
 		case models.DiscoverySourceArmis:
 			hasArmis = true
 		case models.DiscoverySourceNetbox:
