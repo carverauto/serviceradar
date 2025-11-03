5ea76c20c84031885635e4ca25a39fa67d1e0474
Michael Freeman <mfreeman451@gmail.com>
2025-11-02 23:53:41 -0600

    fixing sql queries

---
+++ pkg/db/unified_devices_test.go
@@ -0,0 +1,32 @@
+package db
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestJoinValueTuplesEscapesMaliciousInput(t *testing.T) {
+	t.Parallel()
+
+	values := []string{
+		"default:10.0.0.1",
+		"default:10.0.0.2'); DROP TABLE unified_devices; --",
+	}
+
+	result := joinValueTuples(values)
+
+	require.Contains(t, result, "('default:10.0.0.1')")
+	require.Contains(t, result, "('default:10.0.0.2''); DROP TABLE unified_devices; --')")
+	require.NotContains(t, result, "('default:10.0.0.2'); DROP TABLE unified_devices; --')")
+	require.NotContains(t, result, ",('')")
+}
+
+func TestDedupeStringsSkipsEmptyAndDuplicates(t *testing.T) {
+	t.Parallel()
+
+	values := []string{"", "default:10.0.0.1", "default:10.0.0.1", "default:10.0.0.2"}
+	result := dedupeStrings(values)
+
+	require.Equal(t, []string{"default:10.0.0.1", "default:10.0.0.2"}, result)
+}
