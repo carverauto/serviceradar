e7dbb2b9ded901515641ee068022f4e037e6b66b
Michael Freeman <mfreeman451@gmail.com>
2026-01-14 22:00:04 -0600

    qodo

--- elixir/serviceradar_core/lib/serviceradar/identity/changes/create_tenant_membership.ex
+++ elixir/serviceradar_core/lib/serviceradar/identity/changes/create_tenant_membership.ex
@@ -24,52 +24,57 @@ defmodule ServiceRadar.Identity.Changes.CreateTenantMembership do
   @impl true
   def change(changeset, _opts, _context) do
     Ash.Changeset.after_action(changeset, fn _changeset, user ->
-      create_membership_if_needed(user)
+      try do
+        create_membership_if_needed(user)
+      rescue
+        e ->
+          Logger.error("[CreateTenantMembership] Unexpected error: #{Exception.message(e)}")
+      end
+
       {:ok, user}
     end)
   end
 
+  defp create_membership_if_needed(%{tenant_id: nil} = user) do
+    Logger.warning(
+      "[CreateTenantMembership] User #{user.id} has no tenant_id, skipping membership creation"
+    )
+
+    :ok
+  end
+
   defp create_membership_if_needed(user) do
-    tenant_id = user.tenant_id
+    membership_role = membership_role_for_user(user)
 
-    if is_nil(tenant_id) do
-      Logger.warning(
-        "[CreateTenantMembership] User #{user.id} has no tenant_id, skipping membership creation"
+    user.id
+    |> create_membership(user.tenant_id, membership_role)
+    |> handle_create_result(user, membership_role)
+  end
+
+  defp handle_create_result({:ok, _membership}, user, membership_role) do
+    Logger.info(
+      "[CreateTenantMembership] Created #{membership_role} membership for user #{user.id} in tenant #{user.tenant_id}"
+    )
+
+    :ok
+  end
+
+  defp handle_create_result({:error, %Ash.Error.Invalid{errors: errors}}, user, _role) do
+    if unique_constraint_error?(errors) do
+      Logger.debug(
+        "[CreateTenantMembership] Membership already exists for user #{user.id} in tenant #{user.tenant_id}"
       )
-
-      :ok
     else
-      membership_role = membership_role_for_user(user)
-
-      case create_membership(user.id, tenant_id, membership_role) do
-        {:ok, _membership} ->
-          Logger.info(
-            "[CreateTenantMembership] Created #{membership_role} membership for user #{user.id} in tenant #{tenant_id}"
-          )
-
-          :ok
-
-        {:error, %Ash.Error.Invalid{errors: errors}} ->
-          if unique_constraint_error?(errors) do
-            Logger.debug(
-              "[CreateTenantMembership] Membership already exists for user #{user.id} in tenant #{tenant_id}"
-            )
-
-            :ok
-          else
-            Logger.error(
-              "[CreateTenantMembership] Failed to create membership: #{inspect(errors)}"
-            )
-
-            :ok
-          end
-
-        {:error, reason} ->
-          Logger.error("[CreateTenantMembership] Failed to create membership: #{inspect(reason)}")
-          # Don't fail user creation if membership fails - user can be added manually
-          :ok
-      end
+      Logger.error("[CreateTenantMembership] Failed to create membership: #{inspect(errors)}")
     end
+
+    :ok
+  end
+
+  defp handle_create_result({:error, reason}, _user, _role) do
+    Logger.error("[CreateTenantMembership] Failed to create membership: #{inspect(reason)}")
+    # Don't fail user creation if membership fails - user can be added manually
+    :ok
   end
 
   defp membership_role_for_user(user) do
@@ -96,12 +101,11 @@ defmodule ServiceRadar.Identity.Changes.CreateTenantMembership do
   defp unique_constraint_error?(errors) do
     Enum.any?(errors, fn error ->
       case error do
-        %Ash.Error.Changes.InvalidChanges{message: msg} ->
-          String.contains?(to_string(msg), "unique") or
-            String.contains?(to_string(msg), "already exists")
-
-        %{class: :invalid, message: msg} when is_binary(msg) ->
-          String.contains?(msg, "unique") or String.contains?(msg, "already exists")
+        # AshPostgres wraps unique constraint violations in InvalidAttribute
+        # with constraint_type: :unique in private_vars
+        %Ash.Error.Changes.InvalidAttribute{private_vars: private_vars}
+        when is_list(private_vars) ->
+          Keyword.get(private_vars, :constraint_type) == :unique
 
         _ ->
           false
