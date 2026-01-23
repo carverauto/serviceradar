6330fd03f90e97f608b9850d17d1de0ce29bafa7
Michael Freeman <mfreeman451@gmail.com>
2026-01-23 15:28:35 -0600

    oban schema crash fix

--- elixir/serviceradar_core/priv/repo/migrations/20260123205147_ensure_oban_platform_tables.exs
+++ elixir/serviceradar_core/priv/repo/migrations/20260123205147_ensure_oban_platform_tables.exs
@@ -24,15 +24,20 @@ defmodule ServiceRadar.Repo.Migrations.EnsureObanPlatformTables do
   end
 
   defp copy_public_oban_to_platform do
+    execute("CREATE SEQUENCE IF NOT EXISTS platform.oban_jobs_id_seq")
+
     execute("""
-    CREATE SEQUENCE IF NOT EXISTS platform.oban_jobs_id_seq;
     CREATE TABLE IF NOT EXISTS platform.oban_jobs (
       LIKE public.oban_jobs INCLUDING DEFAULTS INCLUDING CONSTRAINTS INCLUDING INDEXES
-    );
-    ALTER TABLE platform.oban_jobs
-      ALTER COLUMN id SET DEFAULT nextval('platform.oban_jobs_id_seq'::regclass);
-    ALTER SEQUENCE platform.oban_jobs_id_seq OWNED BY platform.oban_jobs.id;
+    )
     """)
+
+    execute("""
+    ALTER TABLE platform.oban_jobs
+      ALTER COLUMN id SET DEFAULT nextval('platform.oban_jobs_id_seq'::regclass)
+    """)
+
+    execute("ALTER SEQUENCE platform.oban_jobs_id_seq OWNED BY platform.oban_jobs.id")
 
     execute("""
     CREATE UNLOGGED TABLE IF NOT EXISTS platform.oban_peers (
@@ -46,12 +51,14 @@ defmodule ServiceRadar.Repo.Migrations.EnsureObanPlatformTables do
   end
 
   defp ensure_platform_oban_supporting_objects do
+    execute("CREATE SEQUENCE IF NOT EXISTS platform.oban_jobs_id_seq")
+
     execute("""
-    CREATE SEQUENCE IF NOT EXISTS platform.oban_jobs_id_seq;
     ALTER TABLE platform.oban_jobs
-      ALTER COLUMN id SET DEFAULT nextval('platform.oban_jobs_id_seq'::regclass);
-    ALTER SEQUENCE platform.oban_jobs_id_seq OWNED BY platform.oban_jobs.id;
+      ALTER COLUMN id SET DEFAULT nextval('platform.oban_jobs_id_seq'::regclass)
     """)
+
+    execute("ALTER SEQUENCE platform.oban_jobs_id_seq OWNED BY platform.oban_jobs.id")
 
     execute("""
     CREATE UNLOGGED TABLE IF NOT EXISTS platform.oban_peers (
