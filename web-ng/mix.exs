1665c2218c933adf6d401250bc137e86be3da501
Michael Freeman <mfreeman451@gmail.com>
2026-02-11 00:37:53 -0600

    chore: adding usage_rules

--- web-ng/mix.exs
+++ web-ng/mix.exs
@@ -12,6 +12,7 @@ defmodule ServiceRadarWebNG.MixProject do
       deps: deps(),
       compilers: [:phoenix_live_view] ++ Mix.compilers(),
       listeners: [Phoenix.CodeReloader]
+      usage_rules: usage_rules()
     ]
   end
 
@@ -114,6 +115,36 @@ defmodule ServiceRadarWebNG.MixProject do
 
       # Phoenix React Server - Server-side rendering for React components
       {:phoenix_react_server, "~> 0.7.3"}
+    ]
+  end
+
+  defp usage_rules do
+    # Example for those using claude.
+    [
+        file: "CLAUDE.md",
+        # rules to include directly in CLAUDE.md
+        # use a regex to match multiple deps, or atoms/strings for specific ones
+        usage_rules: [:ash, ~r/^ash_/],
+        # If your CLAUDE.md is getting too big, link instead of inlining:
+        usage_rules: [:ash, {~r/^ash_/, link: :markdown}],
+        # or use skills
+        skills: [
+        location: ".claude/skills",
+        # build skills that combine multiple usage rules
+        build: [
+            "ash-framework": [
+            # The description tells people how to use this skill.
+            description: "Use this skill working with Ash Framework or any of its extensions. Always consult this when making any domain changes, features or fixes.",
+            # Include all Ash dependencies
+            usage_rules: [:ash, ~r/^ash_/]
+            ],
+            "phoenix-framework": [
+            description: "Use this skill working with Phoenix Framework. Consult this when working with the web layer, controllers, views, liveviews etc.",
+            # Include all Phoenix dependencies
+            usage_rules: [:phoenix, ~r/^phoenix_/]
+            ]
+        ]
+        ]
     ]
   end
 
