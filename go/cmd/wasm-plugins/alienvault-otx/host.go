ba0cd7e70c564aec0f44f9878e77a0866bc4f349
Michael Freeman <mfreeman451@gmail.com>
2026-04-28 19:46:28 -0500

    Use SDK HTTP client in OTX plugin

--- go/cmd/wasm-plugins/alienvault-otx/host.go
+++ go/cmd/wasm-plugins/alienvault-otx/host.go
@@ -1,16 +1,13 @@
-//go:build !tinygo
-
 package main
 
 import (
-	"context"
 	"net/http"
 
 	"code.carverauto.dev/carverauto/serviceradar-sdk-go/sdk"
 )
 
 func doOTXHostHTTPRequest(apiURL, apiKey string, timeoutMS int) (*sdk.HTTPResponse, error) {
-	return sdk.HTTP.DoContext(context.Background(), sdk.HTTPRequest{
+	return sdk.HTTP.Do(sdk.HTTPRequest{
 		Method: http.MethodGet,
 		URL:    apiURL,
 		Headers: map[string]string{
