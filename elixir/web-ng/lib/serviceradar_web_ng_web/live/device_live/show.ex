40296fecbecad0d65fe0fe1cc62b456834695eb2
Michael Freeman <mfreeman451@gmail.com>
2026-05-29 02:19:45 -0500

    Extract device action runtime

--- elixir/web-ng/lib/serviceradar_web_ng_web/live/device_live/show.ex
+++ elixir/web-ng/lib/serviceradar_web_ng_web/live/device_live/show.ex
@@ -4,34 +4,27 @@ defmodule ServiceRadarWebNGWeb.DeviceLive.Show do
 
   import ServiceRadarWebNGWeb.DeviceLive.VirtualizationComponents, only: [virtualization_guests?: 1]
 
-  alias Ash.Error.Invalid
   alias ServiceRadar.Inventory.DevicePubSub
   alias ServiceRadar.Observability.MtrPubSub
-  alias ServiceRadarWebNG.RBAC
   alias ServiceRadarWebNGWeb.Dashboard.Engine
   alias ServiceRadarWebNGWeb.Dashboard.Plugins.Categories, as: CategoriesPlugin
   alias ServiceRadarWebNGWeb.Dashboard.Plugins.Table, as: TablePlugin
   alias ServiceRadarWebNGWeb.DeviceLive.CameraData
   alias ServiceRadarWebNGWeb.DeviceLive.CameraRelayRuntime
-  alias ServiceRadarWebNGWeb.DeviceLive.DeviceFormData
+  alias ServiceRadarWebNGWeb.DeviceLive.DeviceActionRuntime
   alias ServiceRadarWebNGWeb.DeviceLive.DeviceMountAssigns
-  alias ServiceRadarWebNGWeb.DeviceLive.DeviceResourceData
-  alias ServiceRadarWebNGWeb.DeviceLive.DeviceStateData
   alias ServiceRadarWebNGWeb.DeviceLive.DeviceSupplementalData
   alias ServiceRadarWebNGWeb.DeviceLive.DeviceTabRuntime
   alias ServiceRadarWebNGWeb.DeviceLive.FlowRuntime
   alias ServiceRadarWebNGWeb.DeviceLive.InterfaceRuntime
-  alias ServiceRadarWebNGWeb.DeviceLive.IpAliasData
   alias ServiceRadarWebNGWeb.DeviceLive.MetadataData
   alias ServiceRadarWebNGWeb.DeviceLive.MtrRuntime
   alias ServiceRadarWebNGWeb.DeviceLive.NorthboundInterfaceRuntime
   alias ServiceRadarWebNGWeb.DeviceLive.QueryData
-  alias ServiceRadarWebNGWeb.DeviceLive.SNMPCredentialData
   alias ServiceRadarWebNGWeb.DeviceLive.SysmonMetrics
   alias ServiceRadarWebNGWeb.DeviceLive.VirtualizationData
   alias ServiceRadarWebNGWeb.SRQL.Page, as: SRQLPage
 
-  require Ash.Query
   require Logger
 
   @default_limit 50
@@ -468,7 +461,9 @@ defmodule ServiceRadarWebNGWeb.DeviceLive.Show do
     show_stale = socket.assigns.show_stale_aliases
     virtualization_summary = VirtualizationData.load_virtualization_summary(scope, uid)
     has_virtualization_guests = virtualization_guests?(virtualization_summary)
-    {camera_sources, camera_inventory_error} = CameraData.load_sources(scope, uid, device_row, &format_ash_error/1)
+
+    {camera_sources, camera_inventory_error} =
+      CameraData.load_sources(scope, uid, device_row, &DeviceActionRuntime.format_ash_error/1)
 
     supplemental_context = %{
       socket: socket,
@@ -673,152 +668,31 @@ defmodule ServiceRadarWebNGWeb.DeviceLive.Show do
   end
 
   def handle_event("toggle_edit", _params, socket) do
-    if socket.assigns.editing do
-      # Cancel editing - reset form
-      {:noreply,
-       socket
-       |> assign(:editing, false)
-       |> assign(:device_form, to_form(%{}, as: :device))
-       |> assign(:snmp_credential_form, to_form(%{}, as: :snmp))}
-    else
-      # Start editing - populate form with current device data
-      device_row = List.first(Enum.filter(socket.assigns.results, &is_map/1))
-      scope = socket.assigns.current_scope
-
-      device_snmp_credential =
-        socket.assigns.device_snmp_credential ||
-          SNMPCredentialData.load(scope, socket.assigns.device_uid)
-
-      form_data =
-        if device_row do
-          %{
-            "hostname" => Map.get(device_row, "hostname", ""),
-            "ip" => Map.get(device_row, "ip", ""),
-            "type" => Map.get(device_row, "type", ""),
-            "vendor_name" => Map.get(device_row, "vendor_name", ""),
-            "model" => Map.get(device_row, "model", ""),
-            "is_managed" => Map.get(device_row, "is_managed", false),
-            "is_trusted" => Map.get(device_row, "is_trusted", false),
-            "tags" => DeviceFormData.format_tags(Map.get(device_row, "tags"))
-          }
-        else
-          %{}
-        end
-
-      {:noreply,
-       socket
-       |> assign(:editing, true)
-       |> assign(:device_snmp_credential, device_snmp_credential)
-       |> assign(:device_form, to_form(form_data, as: :device))
-       |> assign(
-         :snmp_credential_form,
-         to_form(SNMPCredentialData.form_data(device_snmp_credential), as: :snmp)
-       )}
-    end
+    {:noreply, DeviceActionRuntime.toggle_edit(socket)}
   end
 
   def handle_event("delete_device", _params, socket) do
-    scope = socket.assigns.current_scope
-    device_uid = socket.assigns.device_uid
-
-    deleted_by = DeviceStateData.deleted_by_from_scope(scope)
-
-    case DeviceResourceData.soft_delete(scope, device_uid, deleted_by) do
-      {:ok, _} ->
-        {:noreply,
-         socket
-         |> put_flash(:info, "Device deleted")
-         |> push_patch(to: device_show_path(socket, device_uid))}
-
-      {:error, reason} ->
-        Logger.error("Device delete failed for #{device_uid}: #{inspect(reason)}")
-
-        {:noreply, put_flash(socket, :error, "Failed to delete device: #{format_ash_error(reason)}")}
-    end
+    {:noreply, DeviceActionRuntime.delete_device(socket)}
   end
 
   def handle_event("restore_device", _params, socket) do
-    scope = socket.assigns.current_scope
-    device_uid = socket.assigns.device_uid
-
-    case DeviceResourceData.restore(scope, device_uid) do
-      :ok ->
-        {:noreply,
-         socket
-         |> put_flash(:info, "Device restored")
-         |> push_patch(to: device_show_path(socket, device_uid))}
-
-      {:error, reason} ->
-        Logger.error("Device restore failed for #{device_uid}: #{inspect(reason)}")
-
-        {:noreply, put_flash(socket, :error, "Failed to restore device: #{format_ash_error(reason)}")}
-    end
+    {:noreply, DeviceActionRuntime.restore_device(socket)}
   end
 
   def handle_event("mark_device_active", _params, socket) do
-    update_device_active_state(socket, true)
+    {:noreply, DeviceActionRuntime.mark_active(socket, true)}
   end
 
   def handle_event("mark_device_inactive", _params, socket) do
-    update_device_active_state(socket, false)
+    {:noreply, DeviceActionRuntime.mark_active(socket, false)}
   end
 
   def handle_event("toggle_aliases", _params, socket) do
-    show_stale = not socket.assigns.show_stale_aliases
-    scope = socket.assigns.current_scope
-    device_uid = socket.assigns.device_uid
-
-    {ip_aliases, ip_alias_error} = IpAliasData.load(scope, device_uid, show_stale)
-
-    {:noreply,
-     socket
-     |> assign(:show_stale_aliases, show_stale)
-     |> assign(:ip_aliases, ip_aliases)
-     |> assign(:ip_alias_error, ip_alias_error)}
+    {:noreply, DeviceActionRuntime.toggle_aliases(socket)}
   end
 
   def handle_event("set_availability_source", %{"agent_id" => agent_id}, socket) do
-    scope = socket.assigns.current_scope
-    device_uid = socket.assigns.device_uid
-
-    availability_source_agent_id =
-      agent_id
-      |> to_string()
-      |> String.trim()
-      |> case do
-        "" -> nil
-        value -> value
-      end
-
-    case DeviceResourceData.load(scope, device_uid) do
-      {:ok, device} ->
-        result =
-          device
-          |> Ash.Changeset.for_update(:set_availability_source, %{
-            availability_source_agent_id: availability_source_agent_id,
-            availability_source_profile_id: nil
-          })
-          |> Ash.update(scope: scope)
-
-        case result do
-          {:ok, _updated} ->
-            {:noreply,
-             socket
-             |> put_flash(:info, "Availability source updated")
-             |> push_patch(to: device_show_path(socket, device_uid))}
-
-          {:error, reason} ->
-            {:noreply,
-             put_flash(
-               socket,
-               :error,
-               "Failed to update availability source: #{format_ash_error(reason)}"
-             )}
-        end
-
-      {:error, reason} ->
-        {:noreply, put_flash(socket, :error, "Failed to load device: #{format_ash_error(reason)}")}
-    end
+    {:noreply, DeviceActionRuntime.set_availability_source(socket, agent_id)}
   end
 
   def handle_event(
@@ -826,139 +700,31 @@ defmodule ServiceRadarWebNGWeb.DeviceLive.Show do
         %{"camera_source_id" => camera_source_id, "stream_profile_id" => stream_profile_id} = params,
         socket
       ) do
-    scope = socket.assigns.current_scope
-    insecure_skip_verify = DeviceFormData.parse_bool(params["insecure_skip_verify"]) == true
-
-    cond do
-      not can_view_device?(scope) ->
-        {:noreply, put_flash(socket, :error, "You are not authorized to start a camera relay")}
-
-      not is_nil(socket.assigns.active_camera_relay_session) ->
-        {:noreply, put_flash(socket, :error, "Close the current camera relay before starting another")}
-
-      true ->
-        case CameraRelayRuntime.request_open(camera_source_id, stream_profile_id, scope, insecure_skip_verify) do
-          {:ok, session} ->
-            {:noreply,
-             socket
-             |> clear_flash(:error)
-             |> assign(:active_camera_relay_session, session)
-             |> assign(:last_camera_relay_session, nil)
-             |> tap(fn _socket -> CameraRelayRuntime.schedule_refresh(session.id) end)
-             |> put_flash(:info, "Camera relay requested")}
-
-          {:error, reason} ->
-            {:noreply, put_flash(socket, :error, CameraRelayRuntime.format_error(reason, &format_ash_error/1))}
-        end
-    end
+    {:noreply, DeviceActionRuntime.open_camera_relay(socket, camera_source_id, stream_profile_id, params)}
   end
 
   def handle_event("close_camera_relay", _params, socket) do
-    scope = socket.assigns.current_scope
-    active_session = socket.assigns.active_camera_relay_session
-
-    cond do
-      not can_view_device?(scope) ->
-        {:noreply, put_flash(socket, :error, "You are not authorized to stop a camera relay")}
-
-      is_nil(active_session) ->
-        {:noreply, socket}
-
-      true ->
-        case CameraRelayRuntime.request_close(active_session.id, scope) do
-          {:ok, session} ->
-            {:noreply,
-             socket
-             |> assign(:active_camera_relay_session, session)
-             |> tap(fn _socket -> CameraRelayRuntime.schedule_refresh(session.id) end)
-             |> put_flash(:info, "Camera relay closing")}
-
-          {:error, reason} ->
-            {:noreply, put_flash(socket, :error, CameraRelayRuntime.format_error(reason, &format_ash_error/1))}
-        end
-    end
+    {:noreply, DeviceActionRuntime.close_camera_relay(socket)}
   end
 
   def handle_event("validate_device", %{"device" => params}, socket) do
-    {:noreply, assign(socket, :device_form, to_form(params, as: :device))}
+    {:noreply, DeviceActionRuntime.validate_device(socket, params)}
   end
 
   def handle_event("save_device", %{"device" => params}, socket) do
-    scope = socket.assigns.current_scope
-    device_uid = socket.assigns.device_uid
-
-    case DeviceResourceData.update(scope, device_uid, params) do
-      {:ok, _device} ->
-        {:noreply,
-         socket
-         |> assign(:editing, false)
-         |> put_flash(:info, "Device updated successfully.")
-         |> push_patch(to: ~p"/devices/#{device_uid}")}
-
-      {:error, %Invalid{} = error} ->
-        {:noreply, put_flash(socket, :error, format_ash_error(error))}
-
-      {:error, reason} ->
-        {:noreply, put_flash(socket, :error, "Failed to update device: #{inspect(reason)}")}
-    end
+    {:noreply, DeviceActionRuntime.save_device(socket, params)}
   end
 
   def handle_event("snmp_form_change", %{"snmp" => params}, socket) do
-    current = socket.assigns.snmp_credential_form.source || %{}
-    updated = Map.merge(current, params)
-
-    {:noreply, assign(socket, :snmp_credential_form, to_form(updated, as: :snmp))}
+    {:noreply, DeviceActionRuntime.change_snmp_form(socket, params)}
   end
 
   def handle_event("save_snmp_credentials", %{"snmp" => params}, socket) do
-    scope = socket.assigns.current_scope
-    device_uid = socket.assigns.device_uid
-    editing = not is_nil(socket.assigns.device_snmp_credential)
-    normalized = SNMPCredentialData.normalize_params(params, editing)
-
-    if editing or SNMPCredentialData.params_present?(normalized) do
-      case SNMPCredentialData.upsert(scope, device_uid, normalized) do
-        {:ok, credential} ->
-          {:noreply,
-           socket
-           |> assign(:device_snmp_credential, credential)
-           |> assign(
-             :snmp_credential_form,
-             to_form(SNMPCredentialData.form_data(credential), as: :snmp)
-           )
-           |> put_flash(:info, "SNMP credentials saved")}
-
-        {:error, %Invalid{} = error} ->
-          {:noreply, put_flash(socket, :error, format_ash_error(error))}
-
-        {:error, reason} ->
-          {:noreply, put_flash(socket, :error, "Failed to save SNMP credentials: #{inspect(reason)}")}
-      end
-    else
-      {:noreply, put_flash(socket, :info, "Provide SNMP credentials to create an override")}
-    end
+    {:noreply, DeviceActionRuntime.save_snmp_credentials(socket, params)}
   end
 
   def handle_event("clear_snmp_credentials", _params, socket) do
-    scope = socket.assigns.current_scope
-
-    case socket.assigns.device_snmp_credential do
-      nil ->
-        {:noreply, socket}
-
-      credential ->
-        case SNMPCredentialData.destroy(credential, scope) do
-          :ok ->
-            {:noreply,
-             socket
-             |> assign(:device_snmp_credential, nil)
-             |> assign(:snmp_credential_form, to_form(%{}, as: :snmp))
-             |> put_flash(:info, "SNMP credential override cleared")}
-
-          {:error, reason} ->
-            {:noreply, put_flash(socket, :error, "Failed to clear SNMP credentials: #{inspect(reason)}")}
-        end
-    end
+    {:noreply, DeviceActionRuntime.clear_snmp_credentials(socket)}
   end
 
   def handle_event("switch_tab", %{"tab" => tab}, socket) do
@@ -1160,64 +926,6 @@ defmodule ServiceRadarWebNGWeb.DeviceLive.Show do
     case List.first(Enum.filter(results, &is_map/1)) do
       nil -> nil
       row -> Map.get(row, "ip")
-    end
-  end
-
-  # RBAC helper - check if user can edit devices
-  defp can_view_device?(scope), do: RBAC.can?(scope, "devices.view")
-
-  defp device_show_path(socket, device_uid) do
-    tab =
-      case socket.assigns.active_tab do
-        :details -> nil
-        "details" -> nil
-        other -> to_string(other)
-      end
-
-    params =
-      %{"limit" => socket.assigns.limit}
-      |> maybe_put_param("q", Map.get(socket.assigns.srql || %{}, :query))
-      |> maybe_put_param("tab", tab)
-
-    ~p"/devices/#{device_uid}?#{params}"
-  end
-
-  defp maybe_put_param(params, _key, value) when value in [nil, ""], do: params
-  defp maybe_put_param(params, key, value), do: Map.put(params, key, value)
-
-  defp format_ash_error(%Invalid{errors: errors}) do
-    Enum.map_join(errors, ", ", &format_single_ash_error/1)
-  end
-
-  defp format_ash_error(error), do: inspect(error)
-
-  defp format_single_ash_error(%Ash.Error.Changes.InvalidAttribute{field: field, message: msg}), do: "#{field}: #{msg}"
-
-  defp format_single_ash_error(%Ash.Error.Changes.Required{field: field}), do: "#{field} is required"
-
-  defp format_single_ash_error(%{message: msg}) when is_binary(msg), do: msg
-
-  defp format_single_ash_error(err), do: inspect(err)
-
-  defp update_device_active_state(socket, active?) do
-    scope = socket.assigns.current_scope
-    device_uid = socket.assigns.device_uid
-
-    case DeviceResourceData.set_active(scope, device_uid, active?) do
-      {:ok, _updated} ->
-        message = if active?, do: "Device returned to service", else: "Device marked out of service"
-
-        {:noreply,
-         socket
-         |> put_flash(:info, message)
-         |> push_patch(to: device_show_path(socket, device_uid))}
-
-      {:error, reason} ->
-        action = if active?, do: "return device to service", else: "mark device out of service"
-
-        Logger.error("Device active lifecycle update failed for #{device_uid}: #{inspect(reason)}")
-
-        {:noreply, put_flash(socket, :error, "Failed to #{action}: #{format_ash_error(reason)}")}
     end
   end
 end
