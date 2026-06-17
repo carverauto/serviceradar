7bd836f16243c367263dcaf004875c0ebe7f2d6e
Michael Freeman <mfreeman451@gmail.com>
2026-06-17 05:23:05 -0500

    Bound web-ng database statement runtime

--- elixir/web-ng/config/runtime.exs
+++ elixir/web-ng/config/runtime.exs
@@ -1467,6 +1467,8 @@ if config_env() == :prod do
         nil
     end
 
+  database_statement_timeout_ms = parse_int_env.("DATABASE_STATEMENT_TIMEOUT_MS", 30_000)
+
   # Configure ServiceRadar.Repo from serviceradar_core
   repo_config =
     [
@@ -1479,7 +1481,8 @@ if config_env() == :prod do
       pool_timeout: String.to_integer(System.get_env("DATABASE_POOL_TIMEOUT_MS") || "120000"),
       socket_options: maybe_ipv6,
       parameters: [
-        search_path: System.get_env("CNPG_SEARCH_PATH", "platform, public, ag_catalog")
+        search_path: System.get_env("CNPG_SEARCH_PATH", "platform, public, ag_catalog"),
+        statement_timeout: "#{database_statement_timeout_ms}ms"
       ],
       types: ServiceRadar.PostgresTypes
     ]
