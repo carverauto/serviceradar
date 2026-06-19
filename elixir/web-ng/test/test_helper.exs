64523dbdbcc48e7723ee203547aeaef5c92ceb17
Michael Freeman <mfreeman451@gmail.com>
2026-06-19 17:34:51 -0500

    test(web-ng): remove unreachable db-free test branch

--- elixir/web-ng/test/test_helper.exs
+++ elixir/web-ng/test/test_helper.exs
@@ -35,17 +35,11 @@ db_tests_available? =
         {:error, reason} ->
           message = "web-ng test database unavailable: #{inspect(reason)}"
 
-          cond do
-            require_db_tests? ->
-              raise message
-
-            allow_db_free_tests? ->
-              IO.warn("Continuing without web-ng test database; #{message}")
-              false
-
-            true ->
-              IO.warn("Skipping web-ng tests; #{message}")
-              System.halt(0)
+          if require_db_tests? do
+            raise message
+          else
+            IO.warn("Skipping web-ng tests; #{message}")
+            System.halt(0)
           end
       end
   end
