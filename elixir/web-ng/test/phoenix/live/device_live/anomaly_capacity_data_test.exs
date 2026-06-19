05f1bb95f56942b9e25ee8c85e37afbeeb217354
Michael Freeman <mfreeman451@gmail.com>
2026-06-19 10:05:05 -0500

    fix(web-ng): bound anomaly capacity tasks

--- elixir/web-ng/test/phoenix/live/device_live/anomaly_capacity_data_test.exs
+++ elixir/web-ng/test/phoenix/live/device_live/anomaly_capacity_data_test.exs
@@ -1,7 +1,18 @@
 defmodule ServiceRadarWebNGWeb.DeviceLive.AnomalyCapacityDataTest do
   use ExUnit.Case, async: true
 
+  import ExUnit.CaptureLog
+
   alias ServiceRadarWebNGWeb.DeviceLive.AnomalyCapacityData
+
+  setup_all do
+    if Process.whereis(ServiceRadarWebNG.TaskSupervisor) do
+      :ok
+    else
+      start_supervised!({Task.Supervisor, name: ServiceRadarWebNG.TaskSupervisor})
+      :ok
+    end
+  end
 
   defmodule FakeSRQL do
     @moduledoc false
@@ -21,6 +32,13 @@ defmodule ServiceRadarWebNGWeb.DeviceLive.AnomalyCapacityDataTest do
     def query("in:capacity_forecasts" <> _rest, _opts), do: {:ok, %{"results" => []}}
   end
 
+  defmodule RaisingSRQL do
+    @moduledoc false
+
+    def query("in:events" <> _rest, _opts), do: raise("boom")
+    def query("in:capacity_forecasts" <> _rest, _opts), do: {:ok, %{"results" => []}}
+  end
+
   test "SNMP metric subclasses are grouped into the SNMP anomaly status bucket" do
     data = AnomalyCapacityData.load(FakeSRQL, %{device_uid: "router-1"}, nil)
 
@@ -31,5 +49,17 @@ defmodule ServiceRadarWebNGWeb.DeviceLive.AnomalyCapacityDataTest do
     assert snmp.count == 1
     assert red.status == "normal"
     assert red.count == 0
+  end
+
+  test "SRQL task crashes return an error result without raising" do
+    {data, log} =
+      with_log(fn ->
+        AnomalyCapacityData.load(RaisingSRQL, %{device_uid: "router-1"}, nil)
+      end)
+
+    assert data.status == :error
+    assert data.anomaly_rows == []
+    assert data.anomaly_error =~ "anomaly SRQL task failed"
+    assert log =~ "anomaly SRQL task failed"
   end
 end
