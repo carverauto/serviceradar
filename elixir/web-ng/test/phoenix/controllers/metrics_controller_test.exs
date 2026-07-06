9bf6f05446e830ccb47e850ccd8fb7882269df2c
Michael Freeman <mfreeman451@gmail.com>
2026-07-06 01:50:36 -0500

    Add hosted runtime plan hooks

--- elixir/web-ng/test/phoenix/controllers/metrics_controller_test.exs
+++ elixir/web-ng/test/phoenix/controllers/metrics_controller_test.exs
@@ -2,10 +2,15 @@ defmodule ServiceRadarWebNGWeb.MetricsControllerTest do
   use ServiceRadarWebNGWeb.ConnCase, async: false
 
   test "GET /metrics returns prometheus scrape output", %{conn: conn} do
+    ServiceRadarWebNGWeb.Telemetry.measure_tenant_usage()
+
     conn = get(conn, ~p"/metrics")
 
     assert conn.status == 200
     assert conn |> get_resp_header("content-type") |> List.first() =~ "version=0.0.4"
     assert conn.resp_body =~ "serviceradar_tenant_usage_managed_devices_count"
+    assert conn.resp_body =~ "serviceradar_managed_devices"
+    assert conn.resp_body =~ "serviceradar_collectors_total"
+    assert conn.resp_body =~ "serviceradar_leaf_nodes_total"
   end
 end
