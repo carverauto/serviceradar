5d98d679d4763447ee3b4cdf0c284c375611119c
Michael Freeman <mfreeman@carverauto.dev>
2026-09-04 19:45:11 -0500

    test(web-ng): account for inventory shared-fixture case

--- elixir/web-ng/test/db/networks_live_db_test_helper.exs
+++ elixir/web-ng/test/db/networks_live_db_test_helper.exs
@@ -9,7 +9,7 @@ ExUnit.configure(
   max_cases: 1
 )
 
-expected_selected_tests = 60
+expected_selected_tests = 61
 
 ExUnit.after_suite(fn %{total: total, excluded: excluded, skipped: skipped} ->
   selected = total - excluded - skipped
