50fbd1ef6998a8d31d290d2ed709319e13448482
Michael Freeman <mfreeman451@gmail.com>
2026-09-12 16:47:48 -0500

    no-mistakes(review): create StatefulAlertRule from event details with authorized handlers

--- elixir/web-ng/test/db/networks_live_db_test_helper.exs
+++ elixir/web-ng/test/db/networks_live_db_test_helper.exs
@@ -9,7 +9,7 @@ ExUnit.configure(
   max_cases: 1
 )
 
-expected_selected_tests = 118
+expected_selected_tests = 120
 
 ExUnit.after_suite(fn %{total: total, excluded: excluded, skipped: skipped} ->
   selected = total - excluded - skipped
