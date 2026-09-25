65d49bd6267599a2780651a84d0e93c236228fee
Michael Freeman <mfreeman@carverauto.dev>
2026-09-25 02:21:38 -0500

    no-mistakes(review): guard UUID fallback with Ecto.UUID.cast to return not_found for non-UUID manifest IDs

--- elixir/web-ng/lib/serviceradar_web_ng/dashboards/packages.ex
+++ elixir/web-ng/lib/serviceradar_web_ng/dashboards/packages.ex
@@ -417,10 +417,16 @@ defmodule ServiceRadarWebNG.Dashboards.Packages do
         {:ok, pkg}
 
       {:ok, nil} ->
-        case read_one_package_with_instances(id, scope) do
-          {:ok, nil} -> {:error, :not_found}
-          {:ok, pkg} -> {:ok, pkg}
-          {:error, error} -> {:error, error}
+        case Ecto.UUID.cast(id) do
+          {:ok, _} ->
+            case read_one_package_with_instances(id, scope) do
+              {:ok, nil} -> {:error, :not_found}
+              {:ok, pkg} -> {:ok, pkg}
+              {:error, error} -> {:error, error}
+            end
+
+          :error ->
+            {:error, :not_found}
         end
 
       {:error, error} ->
